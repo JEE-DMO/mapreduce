159fc41815cb8a151abc207cee827fcc6bd1330e
Hicham Bellahcene <hbellahc@gmail.com>
2018-10-06 06:43:30 +0200

    'revision'

--- src/main/java/AddressGeocoder.java
+++ src/main/java/AddressGeocoder.java
@@ -10,31 +10,36 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.logging.Logger;
 
-public class AnagramFinder {
+public class AddressGeocoder {
 
     public static class Mapper extends org.apache.hadoop.mapreduce.Mapper<LongWritable, Text, Text, Text> {
 
-        private Text sortedText = new Text();
-        private Text outputValue = new Text();
-        private Logger logger = Logger.getLogger("Mapper");
+        private Text geoCodeText = new Text();
+        private Text addressText = new Text();
 
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer tokenizer = new StringTokenizer(value.toString(),
-                    " \t\n\r\f,.:()!?", false);
 
-            logger.info("Value " + value.toString());
+            geoCodeText.set(GeoUtil.geoCode(value.toString()));
+            addressText.set(value);
+            context.write(geoCodeText, addressText);
 
-            while (tokenizer.hasMoreTokens()) {
-                //todo
-            }
         }
-
 
 
     }
 
+    public static class Combiner extends org.apache.hadoop.mapreduce.Reducer<Text, Text, Text, Text> {
+
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            Set<Text> uniques = new HashSet<Text>();
+            for (Text value : values) {
+                if (uniques.add(value)) {
+                    context.write(key, value);
+                }
+            }
+        }
+    }
 
     public static class Reducer extends org.apache.hadoop.mapreduce.Reducer<Text, Text, IntWritable, Text> {
 
@@ -42,7 +47,24 @@ public class AnagramFinder {
         private Text outputValue = new Text();
 
         protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            //TODO
+            Set<Text> uniques = new HashSet<Text>();
+            int size = 0;
+            StringBuilder builder = new StringBuilder();
+            builder.append(key + " -> ");
+            for (Text value : values) {
+                if (uniques.add(value)) {
+                    size++;
+                    builder.append(value.toString());
+                    builder.append(',');
+                }
+            }
+            builder.setLength(builder.length() - 1);
+
+            if (size > 1) {
+                count.set(size);
+                outputValue.set(builder.toString());
+                context.write(count, outputValue);
+            }
         }
 
     }
@@ -50,9 +72,9 @@ public class AnagramFinder {
     public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException {
 
         Configuration conf = new Configuration();
-        Job job = new Job(conf, "Anagram Finder");
+        Job job = new Job(conf, "Address Geocoder");
 
-        job.setJarByClass(AnagramFinder.class);
+        job.setJarByClass(AddressGeocoder.class);
 
 
         org.apache.hadoop.mapreduce.lib.input.FileInputFormat.addInputPath(job, new Path(args[0]));
